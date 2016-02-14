a14820912074c32aa19d9e37ad524a7cbe79bce8
Terrell V (MirPresT) <terrell.vest@gmail.com>
2016-02-14 13:33:11 -0800

    begin refactoring for view on mobile devices. Header formatting done for smallest screen size

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+
 // SASS SCSS
-gulp.task('scss')
+gulp.task('scss', function(){
+  gulp.src('src/assets/stylesheets/main.scss',{base:'src'})
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('dist/'))
+  .pipe(browserSync.stream());
+})
 
 // browserSync
-gulp.task('browserSync',[],function(){
+gulp.task('browserSync',['watch'],function(){
   browserSync.init({
     server:{
       baseDir: './'
@@ -14,6 +24,10 @@ gulp.task('browserSync',[],function(){
 });
 
 
+// watch file changes
+gulp.task('watch', ['scss'], function() {
+  gulp.watch('src/assets/stylesheets/**/*.scss', ['scss']);
+});
 
 // default sequence when typing gulp command
-gulp.task('default',['browserSync']);
+gulp.task('default',['scss','watch','browserSync']);
